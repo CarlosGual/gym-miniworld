dc9c85ca756313b6dd1f1ed9a3c31789fba67edc
carlos <carlos.gutierrezalva@uah.es>
2023-02-07 13:19:50 +0100

    change in pyglet version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     install_requires=[
         'gym<=0.17.2',
         'numpy>=1.10.0',
-        'pyglet>=1.5.11',
+        'pyglet == 1.5.27',
     ],
     # Include textures and meshes in the package
     include_package_data=True
